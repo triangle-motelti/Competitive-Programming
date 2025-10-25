8e369cb4d2aa5f8ab0c684558be67c794c797ee6
triangle-motelti <mteltipr03@gmail.com>
2025-10-25 20:40:22 +0100

    determine the maximum total number of slices Hao can eat if he always chooses the partition optimally

---
+++ Codeforces/Pizza-Time.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ll n;cin>>n;
+	ll a, res;
+	while (cin>>a) {
+	    res = ((a - 1) / 2);
+	    cout<<res<<endl;
+	}
+	return 0;
+}
+
