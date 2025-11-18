a72ddfd7f946f94261cfd7c825e3d3a1a9afd5ad
triangle-motelti <mteltipr03@gmail.com>
2025-11-19 00:06:35 +0100

    Codeforces Round 162 (Div. 2)

---
+++ Codeforces/A.Colorful-Stones.cpp
@@ -0,0 +1,36 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+void solve() {
+    string s, t; cin>>s; cin>>t;
+    
+    int ans = 0;
+    for (char c : t) {
+        if (s[ans] == c) {
+            ans++;
+        }
+    }
+    cout<<ans+1<<endl;
+}
+
+int main() {
+    fast_io;
+    solve();
+    
+}
