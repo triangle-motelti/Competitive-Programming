05c9ee95115b26abf0da933b5a81e0c852561ac7
triangle-motelti <mteltipr03@gmail.com>
2025-11-19 14:26:36 +0100

    Codeforces Round 424 (Div. 2, rated, based on VK Cup Finals)

---
+++ Codeforces/A.Unimodal-Array.cpp
@@ -0,0 +1,48 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+void solve() {
+    int n; cin>>n;
+    
+    vector<int> a(n);
+    forn(i, n)
+        cin>>a[i];
+    
+    int i = 1;
+
+    while (i < n && a[i] > a[i-1])
+        i++;
+
+    while (i < n && a[i] == a[i-1])
+        i++;
+
+    while (i < n && a[i] < a[i-1]) 
+        i++;
+    
+    if (i == n)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
+
+int main() {
+    fast_io;
+    solve();
+    
+}
