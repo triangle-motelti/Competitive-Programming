629ce825ae7f447a43fe2197d725e3f9f3bf7173
triangle-motelti <mteltipr03@gmail.com>
2025-11-19 19:55:30 +0100

    Starters 213 (Rated) div 4

---
+++ Codechef/Exun-and-the-Pizzas.cpp
@@ -0,0 +1,33 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+void solve() {
+    int n, k, r; cin>>n>>k>>r;
+    
+    int r1 = n - k;
+    int ans = r * r1;
+    
+    cout<<ans<<endl;
+}
+
+int main() {
+    fast_io;
+    solve();
+    
+}
