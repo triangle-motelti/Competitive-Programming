d595ef072dc447e3b098b79e3d327ff7071418bd
triangle-motelti <mteltipr03@gmail.com>
2025-11-21 23:05:43 +0100

    first problem in UVa

---
+++ UVa-Online-Judge/10110-Light,-more-light.cpp
@@ -0,0 +1,39 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+void solve() {
+    
+    ll n;
+    while (cin>>n && n != 0) {
+        ll ans = sqrt(n);
+        // cout<<(ans*ans)<<endl;
+        if ((ans*ans) == n)
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+    }
+}
+
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1051
+
+int main() {
+    fast_io;
+    solve();
+    
+}
