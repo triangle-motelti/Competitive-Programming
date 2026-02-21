1ab87cc9d76a5e6a9207a9e71d7b079daad02a23
triangle-motelti <mteltipr03@gmail.com>
2026-02-21 23:38:17 +0000

    Codeforces Round 368 (Div. 2)

---
+++ Codeforces/A.Brain's-Photos.cpp
@@ -0,0 +1,50 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+// #define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+ 
+typedef long long ll;
+ 
+template <typename T>
+bool is_odd(T n) { return n % 2 != 0; }
+ 
+class Solution {
+public:
+    void solve() {
+        ll n, m; cin>>n>>m;
+        
+        // int ans = 0;
+        
+        bool c = false;
+
+        forn(i, n) {
+            forn(j, m) {
+                char ch; cin>>ch;
+    
+                if (ch == 'C' || ch == 'M' || ch == 'Y')
+                    c = true;
+            }
+        }
+    
+        if (c)
+            cout << "#Color" << endl;
+        else
+            cout << "#Black&White" << endl;
+    }
+};
+ 
+int main() {
+	Solution().solve();
+}
