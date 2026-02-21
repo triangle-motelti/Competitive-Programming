fa8714780ed76f267311f9847d925b9e131e3635
triangle-motelti <mteltipr03@gmail.com>
2026-02-21 23:48:09 +0000

    Codeforces Beta Round 69 (Div. 2 Only)

---
+++ Codeforces/A.Panoramix's-Prediction.cpp
@@ -0,0 +1,55 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+template <typename T>
+bool is_odd(T n) { return n % 2 != 0; }
+
+template <typename T>
+bool isPrime(T n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    
+    return true;
+}
+
+class Solution {
+public:
+    void solve() {
+        ll n, m; cin>>n>>m;
+        
+        int ans = n + 1;
+        while (!isPrime(ans))
+            ans++;
+        
+        if (ans == m)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+};
+
+int main() {
+    fast_io;
+    Solution().solve();
+}
