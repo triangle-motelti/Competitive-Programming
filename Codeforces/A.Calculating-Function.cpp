0200f94c6cc428741a90b5f146d50406633eb321
triangle-motelti <mteltipr03@gmail.com>
2026-02-21 23:45:09 +0000

    Codeforces Round 277 (Div. 2)

---
+++ Codeforces/A.Calculating-Function.cpp
@@ -0,0 +1,42 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+template <typename T>
+bool is_odd(T n) { return n % 2 != 0; }
+
+class Solution {
+public:
+    void solve() {
+        ll n; cin>>n;
+        
+        ll ans = 0;
+        if (!is_odd(n))
+            ans = n/2;
+        else
+            ans = (n-1) / 2-n;
+        
+        cout<<ans<<endl;
+        
+    }
+};
+
+int main() {
+    fast_io;
+    Solution().solve();
+}
