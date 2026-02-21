1e366e4d067092ebde4cd038d9d8d00e56fa4d17
triangle-motelti <mteltipr03@gmail.com>
2026-02-21 23:56:14 +0000

    Codeforces Global Round 31 (Div. 1 + Div. 2)

---
+++ Codeforces/A.Carnival-Wheel.cpp
@@ -0,0 +1,50 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+template <typename T>
+bool is_odd(T n) { return n % 2 != 0; }
+
+class Solution {
+public:
+    void solve() {
+        int t; cin>>t;
+    
+        while (t--) {
+            ll l, a, b; cin>>l>>a>>b;
+            vector<bool> v(l, false);
+    
+            int cur = a;
+            int mx = a;
+            while (!v[cur]) {
+                v[cur] = true;
+                
+                if (cur > mx)
+                    mx= cur;
+    
+                cur = (cur + b) % l;
+            }
+            cout<<mx<<endl;
+        }
+    }
+};
+
+int main() {
+    fast_io;
+    Solution().solve();
+}
