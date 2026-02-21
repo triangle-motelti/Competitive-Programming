c687d4d1d723818e891a0f4aee2a14312e571960
triangle-motelti <mteltipr03@gmail.com>
2026-02-21 23:45:58 +0000

    Codeforces Beta Round 1

---
+++ Codeforces/A.Theatre-Square.cpp
@@ -0,0 +1,38 @@
+/* ******************************************************* */
+/*     triangle :  https://github.com/triangle-motelti     */
+/* ******************************************************* */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define x first
+#define y second
+#define pb push_back
+#define sort(a) sort(a.begin(), a.end());
+#define revsort(a) sort(a.begin(), a.end(), greater<int>());
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define swap(a, b) (a) ^= (b), (b) ^= (a), (a) ^= (b);
+#define sz(a) int((a).size())
+#define endl '\n'
+
+typedef long long ll;
+
+template <typename T>
+bool is_odd(T n) { return n % 2 != 0; }
+
+class Solution {
+public:
+    void solve() {
+        ll n, m, a; cin>>n>>m>>a;
+        
+        // 6 6 4 ==> 4
+        ll len = (n + a - 1) /a;
+        ll wid = (m + a - 1) /a;
+        cout<<len*wid<<endl;
+    }
+};
+
+int main() {
+    fast_io;
+    Solution().solve();
+}
